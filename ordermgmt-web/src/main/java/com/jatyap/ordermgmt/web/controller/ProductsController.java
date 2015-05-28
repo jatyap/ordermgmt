23c744c5dad7ea4fd7fb95d3623403ef49563443
jatyap <jatyap@gmail.com>
2015-05-28 22:06:02 +0800

    Created web front end for products

--- ordermgmt-web/src/main/java/com/jatyap/ordermgmt/web/controller/ProductsController.java
+++ ordermgmt-web/src/main/java/com/jatyap/ordermgmt/web/controller/ProductsController.java
@@ -4,35 +4,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jatyap.ordermgmt.entity.Product;
-import com.jatyap.ordermgmt.repository.ProductRepository;
+import com.jatyap.ordermgmt.service.ProductService;
 
 @Controller
 public class ProductsController {
 
 	@Autowired
-	private ProductRepository productRepo;
-
+	private ProductService productService;
+	
 	@RequestMapping("/products")
 	public String productsMain(Model model) {
-		Iterable<Product> productList = this.productRepo.findAll();
+		Iterable<Product> productList = this.productService.getProductList();
 		model.addAttribute("product", new Product());
 		model.addAttribute("products", productList);
 		return "product/products";
 	}
+	
+	@RequestMapping(value="/products/add", method=RequestMethod.GET)
+	public String addProduct(Model model) {
+		model.addAttribute("product", new Product());
+		return "product/add";
+	}
 
-	@RequestMapping("/saveProduct")
-	public ModelAndView saveProduct(Product product, Model model) {
-		this.productRepo.save(product);
+	@RequestMapping(value="/products/add", method=RequestMethod.POST)
+	public ModelAndView addProduct(Product product, Model model) {
+		this.productService.addProduct(product);
 		return new ModelAndView("redirect:/products");
 	}
 	
-	@RequestMapping("/deleteProduct")
+	@RequestMapping(value = "/products/modify")
+	public String modifyProduct(@RequestParam(value="productId", required=true) String productId, 
+			Model model){
+		Product productToModify = this.productService.getProduct(productId);
+		model.addAttribute("product", productToModify);
+		return "product/modify";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value = "/products/modify", params = { "save" })
+	public ModelAndView modifyProduct(Product product, Model model){
+		this.productService.updateProduct(product);
+		return new ModelAndView("redirect:/products");
+	}
+	
+	@RequestMapping("/products/delete")
 	public ModelAndView deleteProduct(@RequestParam(value="productId", required=true) String productId){
-		this.productRepo.delete(productId);
+		this.productService.deleteProduct(productId);
 		return new ModelAndView("redirect:/products");
 	}
 
