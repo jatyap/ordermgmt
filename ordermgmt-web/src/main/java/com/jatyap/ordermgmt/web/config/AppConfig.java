0f7aad353019b3c9557a988b196ee02f0f153552
jatyap <jatyap@gmail.com>
2015-05-28 22:04:39 +0800

    Added ProductService bean to Appconfig

--- ordermgmt-web/src/main/java/com/jatyap/ordermgmt/web/config/AppConfig.java
+++ ordermgmt-web/src/main/java/com/jatyap/ordermgmt/web/config/AppConfig.java
@@ -1,8 +1,17 @@
 package com.jatyap.ordermgmt.web.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.jatyap.ordermgmt.service.DefaultProductServiceImpl;
+import com.jatyap.ordermgmt.service.ProductService;
 
+@Configuration
 public class AppConfig {
 
+	@Bean
+	public ProductService productService(){
+		return new DefaultProductServiceImpl();
+	}
 
 }
