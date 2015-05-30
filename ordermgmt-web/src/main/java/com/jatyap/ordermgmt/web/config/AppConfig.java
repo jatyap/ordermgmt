66929b111f26ec1ee369d04bd2bd3e56d5a54429
jatyap <jatyap@gmail.com>
2015-05-30 15:55:35 +0800

    Removed bean declaration for ProductService in the configuration.

--- ordermgmt-web/src/main/java/com/jatyap/ordermgmt/web/config/AppConfig.java
+++ ordermgmt-web/src/main/java/com/jatyap/ordermgmt/web/config/AppConfig.java
@@ -1,17 +1,8 @@
 package com.jatyap.ordermgmt.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.jatyap.ordermgmt.service.DefaultProductManager;
-import com.jatyap.ordermgmt.service.ProductService;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public ProductService productService(){
-		return new DefaultProductManager();
-	}
 
 }
